48787c9885f3f8238f5b4c620a245bcfd9971646
Oczane Rivera <oczane.rivera@sema4.com>
2019-10-11 17:10:53 -0400

    Create some templates

---
+++ templates.js
@@ -0,0 +1,30 @@
+/**
+ * The following comments are like templates that you can copy/paste
+ * over into your code.
+ */
+
+// Single comment.
+
+/**
+ *  A one-sentence summary here
+ *
+ * If needed, some additional description in a separate paragraph.
+ * this comment syntax is great for components, publich methods, classes
+ * and other complex or public modules.
+ *
+ * Params:
+ * Variable   Description
+ * (Variable) Optional Variable description
+ */
+
+/*
+  Brief summary of your function/method here.
+  It can span several lines. 
+ */
+
+/*
+ * This is another style of
+ * multiline comment.
+ * The IDE shoud provide the extra *s
+ * on every new line for you.
+ */
